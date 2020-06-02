690ba1c387bc649bcb00529684a3e82f7941664e
Akari207 <akari.kanbe@unique-log.com>
2020-06-03 06:14:21 +0900

    初期コミット

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+/*
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.sass']
 })
+*/
+
+@Component({
+   selector: 'my-app',
+   template: `<h1>Hello {{title}}</h1>`,
+})
+
 export class AppComponent {
-  title = 'angular-study';
+   title = 'angular-study';
 }
